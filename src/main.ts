610e219713828b77cc58fdc87071eed174ae72ac
hiepnguyen223 <hiepnguyenn223@gmail.com>
2022-06-12 08:42:55 +0700

    Deploy to heroku

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     await prismaService.enableShutdownHooks(app);
 
     app.use(cookieParser());
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
